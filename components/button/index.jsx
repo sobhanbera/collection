4b6d767ee128d3ea578b86954cb34bf54f569bc3
vidhanshu <vidhanshu7218555039@gmail.com>
2022-01-23 12:14:43 +0530

    className error

--- components/button/index.jsx
+++ components/button/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../../styles/components/button/index.module.scss'
 function Button({onClickFun,btnTitle}) {
-    return <button className={styles.btn} onClick={onClickFun?onClickFun:null}><span class="text">{btnTitle}</span></button>;
+    return <button className={styles.btn} onClick={onClickFun?onClickFun:null}><span className="text">{btnTitle}</span></button>;
 }
 
 export default Button;
